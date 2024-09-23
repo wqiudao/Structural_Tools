a2ad9f4cf05cf6af5549716e03520feee946b266
wqiudao <33651085+wqiudao@users.noreply.github.com>
2024-09-23 15:53:50 +0800

    Update color_atoms_by_atom_plddts.py

--- color_atoms_by_atom_plddts.py
+++ color_atoms_by_atom_plddts.py
@@ -6,6 +6,8 @@ Set colors for atoms in the loaded PDB file based on the atom_plddts values from
 Parameters:
 - `json_file`: Path to the JSON file containing the atom_plddts values
 
+The JSON file is located in PyMOL's current working directory; otherwise, the full path is required.
+
 color_atoms_by_plddt json_file
 
 """
